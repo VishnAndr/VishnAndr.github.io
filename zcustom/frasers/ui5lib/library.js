3120b5ecc4e098b28ace99c15a1f0988e3e39168
Andrei Vishnevsky <vishneuski@gmail.com>
2018-06-02 23:16:38 +1000

    1.0.38

--- zcustom/frasers/ui5lib/library.js
+++ zcustom/frasers/ui5lib/library.js
@@ -25,7 +25,7 @@ sap.ui.define([
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : "zcustom.frasers.ui5lib",
-		dependencies : [], //["sap.ui.core", "sap.m" , "sap.ui.unified"],
+		dependencies : [],
 		types: [],
 		interfaces: [],
 		controls: [
@@ -33,8 +33,8 @@ sap.ui.define([
 			"zcustom.frasers.ui5lib.control.ZFeederEnhancementPane"
 		],
 		elements: [],
-		version: "0.1.0",
-		noLibraryCSS: true
+		noLibraryCSS: true,
+		version: "0.1.0"
 	});
 
 	return zcustom.frasers.ui5lib;
