1c28f9c828495b8d36ea599fb93492a33dc118eb
Andrei Vishnevsky <vishneuski@gmail.com>
2018-08-22 00:00:20 +1000

    atta 1.0.85

--- zcustom/demo/ui5lib/control/ZThumbnailTileRenderer-dbg.js
+++ zcustom/demo/ui5lib/control/ZThumbnailTileRenderer-dbg.js
@@ -1,10 +1,10 @@
 sap.ui.define([
 	"sap/m/library",
 	"jquery.sap.global",
-	"sap/m/GenericTileRenderer"
-], function (library, jQuery, GenericTileRenderer) {
+	"sap/m/GenericTileRenderer",
+	"sap/ui/core/Renderer"
+], function (library, jQuery, GenericTileRenderer, Renderer) {
 	"use strict";
-
 
 	// shortcut for sap.m.GenericTileMode
 	var GenericTileMode = library.GenericTileMode;
@@ -14,108 +14,105 @@ sap.ui.define([
 
 	// shortcut for sap.m.LoadState
 	var LoadState = library.LoadState;
-	
-	var ZThumbnailTileRenderer = GenericTileRenderer.extend("zcustom.demo.ui5lib.control.ZThumbnailTileRenderer", /** @lends zcustom.demo.ui5lib.control.ZThumbnailTile.prototype */ {
-		metadata: {
-			library: "zcustom.demo.ui5lib"
-		}
-	});
 
-	ZThumbnailTileRenderer.render = function (oRm, oControl) {
-		// Write the HTML into the render manager.
-		var sTooltipText = oControl._getTooltipText();
-		var sAriaText = oControl._getAriaText();
-		var sHeaderImage = oControl.getHeaderImage();
-		var bHasPress = oControl.hasListeners("press");
-		var sState = oControl.getState();
-		var sScope = oControl.getScope();
-		var sStateClass = jQuery.sap.encodeCSS("sapMGTState" + sState);
-		var sScopeClass = jQuery.sap.encodeCSS("sapMGTScope" + sScope);
+	debugger;
+	var ZThumbnailTileRenderer = GenericTileRenderer.extend("zcustom.demo.ui5lib.control.ZThumbnailTileRenderer", {
+		render: function (oRm, oControl) {
+			// Write the HTML into the render manager.
+			var sTooltipText = oControl._getTooltipText();
+			var sAriaText = oControl._getAriaText();
+			var sHeaderImage = oControl.getHeaderImage();
+			var bHasPress = oControl.hasListeners("press");
+			var sState = oControl.getState();
+			var sScope = oControl.getScope();
+			var sStateClass = jQuery.sap.encodeCSS("sapMGTState" + sState);
+			var sScopeClass = jQuery.sap.encodeCSS("sapMGTScope" + sScope);
 
-		oRm.write("<div");
-		oRm.writeControlData(oControl);
-		if (sTooltipText) {
-			oRm.writeAttributeEscaped("title", sTooltipText);
-		}
-		oRm.addClass("sapMGT");
-		oRm.addClass(sStateClass);
-		oRm.addClass(sScopeClass);
-		// render actions view for SlideTile actions scope
-		if (sScope !== GenericTileScope.Actions && oControl._bShowActionsView) {
-			oRm.addClass("sapMGTScopeActions");
-		}
-		oRm.addClass(oControl.getFrameType());
-		if (bHasPress) {
-			oRm.writeAttribute("role", "button");
-		} else {
-			oRm.writeAttribute("role", "presentation");
-		}
-		oRm.writeAttributeEscaped("aria-label", sAriaText);
-		if (sState !== LoadState.Disabled) {
-			oRm.addClass("sapMPointer");
-			oRm.writeAttribute("tabindex", "0");
-		}
-		if (oControl.getBackgroundImage()) {
-			oRm.write(" style='background-image:url(");
-			oRm.writeEscaped(oControl.getBackgroundImage());
-			oRm.write(");'");
-			oRm.addClass("sapMGTBackgroundImage");
-		}
-		if (oControl.getMode() === GenericTileMode.HeaderMode) {
-			oRm.addClass("sapMGTHeaderMode");
-		}
-		oRm.writeClasses();
-		oRm.write(">");
-
-		if (oControl.getHeader()) {
 			oRm.write("<div");
-			oRm.addClass("sapMGTHdrContent");
-			oRm.addClass(oControl.getFrameType());
+			oRm.writeControlData(oControl);
 			if (sTooltipText) {
 				oRm.writeAttributeEscaped("title", sTooltipText);
 			}
+			oRm.addClass("sapMGT");
+			oRm.addClass(sStateClass);
+			oRm.addClass(sScopeClass);
+			// render actions view for SlideTile actions scope
+			if (sScope !== GenericTileScope.Actions && oControl._bShowActionsView) {
+				oRm.addClass("sapMGTScopeActions");
+			}
+			oRm.addClass(oControl.getFrameType());
+			if (bHasPress) {
+				oRm.writeAttribute("role", "button");
+			} else {
+				oRm.writeAttribute("role", "presentation");
+			}
+			oRm.writeAttributeEscaped("aria-label", sAriaText);
+			if (sState !== LoadState.Disabled) {
+				oRm.addClass("sapMPointer");
+				oRm.writeAttribute("tabindex", "0");
+			}
+			if (oControl.getBackgroundImage()) {
+				oRm.write(" style='background-image:url(");
+				oRm.writeEscaped(oControl.getBackgroundImage());
+				oRm.write(");'");
+				oRm.addClass("sapMGTBackgroundImage");
+			}
+			if (oControl.getMode() === GenericTileMode.HeaderMode) {
+				oRm.addClass("sapMGTHeaderMode");
+			}
 			oRm.writeClasses();
 			oRm.write(">");
-			if (sHeaderImage) {
-				oRm.renderControl(oControl._oImage);
+
+			if (oControl.getHeader()) {
+				oRm.write("<div");
+				oRm.addClass("sapMGTHdrContent");
+				oRm.addClass(oControl.getFrameType());
+				if (sTooltipText) {
+					oRm.writeAttributeEscaped("title", sTooltipText);
+				}
+				oRm.writeClasses();
+				oRm.write(">");
+				if (sHeaderImage) {
+					oRm.renderControl(oControl._oImage);
+				}
+
+				this._renderHeader(oRm, oControl);
+
+				if (oControl.getSubheader()) {
+					this._renderSubheader(oRm, oControl);
+				}
+				oRm.write("</div>");
 			}
 
-			this._renderHeader(oRm, oControl);
+			oRm.write("<div");
+			oRm.addClass("sapMGTContent");
+			oRm.writeClasses();
+			oRm.writeAttribute("id", oControl.getId() + "-content");
+			oRm.write(">");
+			var aTileContent = oControl.getTileContent();
+			var iLength = aTileContent.length;
+			for (var i = 0; i < iLength; i++) {
+				oControl._checkFooter(aTileContent[i], oControl);
+				oRm.renderControl(aTileContent[i]);
+			}
+			oRm.write("</div>");
 
-			if (oControl.getSubheader()) {
-				this._renderSubheader(oRm, oControl);
+			if (sState !== LoadState.Loaded) {
+				this._renderStateOverlay(oRm, oControl, sTooltipText);
+			}
+
+			if (sState !== LoadState.Disabled) {
+				this._renderHoverOverlay(oRm, oControl);
+				this._renderFocusDiv(oRm, oControl);
+			}
+
+			if (sScope === GenericTileScope.Actions) {
+				this._renderActionsScope(oRm, oControl);
 			}
 			oRm.write("</div>");
 		}
-		
-		oRm.write("<div");
-		oRm.addClass("sapMGTContent");
-		oRm.writeClasses();
-		oRm.writeAttribute("id", oControl.getId() + "-content");
-		oRm.write(">");
-		var aTileContent = oControl.getTileContent();
-		var iLength = aTileContent.length;
-		for (var i = 0; i < iLength; i++) {
-			oControl._checkFooter(aTileContent[i], oControl);
-			oRm.renderControl(aTileContent[i]);
-		}
-		oRm.write("</div>");
-
-		if (sState !== LoadState.Loaded) {
-			this._renderStateOverlay(oRm, oControl, sTooltipText);
-		}
-
-		if (sState !== LoadState.Disabled) {
-			this._renderHoverOverlay(oRm, oControl);
-			this._renderFocusDiv(oRm, oControl);
-		}
-
-		if (sScope === GenericTileScope.Actions) {
-			this._renderActionsScope(oRm, oControl);
-		}
-		oRm.write("</div>");
-	};
+	});
 
 	return ZThumbnailTileRenderer;
 
-}, /* bExport= */ true);
+});
